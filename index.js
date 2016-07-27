159e74a042bf46750d9f21aec998bde075805e33
Kurtis Kemple <kurtiskemple@gmail.com>
2016-07-27 09:43:23 -0700

    update config to use colors object structure

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-'use strict';
+'use strict'
 
-const foregroundColor = '#eff0eb';
-const backgroundColor = '#292824';
-const black = backgroundColor;
-const slate = '#7e7966';
-const lightSlate = '#afa067';
-const mutedYellow = '#e7d596';
-const orange = '#df7c32';
-const lightOrange = '#ffb37b';
-const gray = '#c59a7c';
-const brightBlack = '#c59a7c';
-const brightWhite = foregroundColor;
+const foregroundColor = '#eff0eb'
+const backgroundColor = '#292824'
+const black = backgroundColor
+const slate = '#7e7966'
+const lightSlate = '#afa067'
+const mutedYellow = '#e7d596'
+const orange = '#df7c32'
+const lightOrange = '#ffb37b'
+const gray = '#c59a7c'
+const brightBlack = '#c59a7c'
+const brightWhite = foregroundColor
+
+const colors = {
+	black: black,
+	red: slate,
+	green: slate,
+	pink: lightSlate,
+	yellow: mutedYellow,
+	blue: orange,
+	magenta: lightOrange,
+	cyan: gray,
+	white: foregroundColor,
+	lightBlack: brightBlack,
+	lightRed: lightSlate,
+	lightGreen: lightSlate,
+	lightPink: lightOrange,
+	lightYellow: mutedYellow,
+	lightBlue: orange,
+	lightMagenta: lightOrange,
+	lightCyan: gray,
+	lightWhite: foregroundColor
+}
 
 exports.decorateConfig = config => {
 	return Object.assign({}, config, {
@@ -18,29 +39,24 @@ exports.decorateConfig = config => {
 		foregroundColor,
 		borderColor: '#222430',
 		cursorColor: '#df7c32',
-		colors: [
-			black,
-			slate,
-      lightSlate,
-      mutedYellow,
-			orange,
-			lightOrange,
-			gray,
-
-			// bright
-			brightBlack,
-			slate,
-      lightSlate,
-      mutedYellow,
-			orange,
-			lightOrange,
-			brightWhite
-		],
+		colors: colors,
 		css: `
 			${config.css || ''}
 			.tab_active:before {
-				border-color: rgba(223, 124, 50, 0.25);
+				border-color: rgba(223, 124, 50, 0.25)
 			}
 		`
-	});
-};
+	})
+}
+
+exports.middleware = () => (next) => (action) => {
+	switch (action.type) {
+		case 'CONFIG_LOAD':
+		case 'CONFIG_RELOAD':
+		action.config.foregroundColor = foregroundColor
+		action.config.backgroundColor = backgroundColor
+		action.config.cursorColor = orange
+		action.config.colors = colors
+	}
+	next(action)
+}
